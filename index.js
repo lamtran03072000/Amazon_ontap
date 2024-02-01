e685e30cfa4885d6e47d2a8d15f632ecb0a80959
lamtran03072000 <tranhoanglam03072000@gmail.com>
2024-02-01 13:34:54 +0700

    c1_b1_xaylayout

---
+++ index.js
@@ -0,0 +1,25 @@
+import { dataPhone } from './dataPhone.js';
+console.log('dataPhone: ', dataPhone);
+let content = '';
+for (let i = 0; i < dataPhone.length; i++) {
+  content += ` 
+  <div class="col-3">
+  <div class="card h-100">
+    <img
+      src="./image/${dataPhone[i].img}"
+      class="card-img-top img-product"
+      alt="..."
+    />
+    <div class="card-body">
+      <h5 class="card-title">${dataPhone[i].name}</h5>
+      <p class="card-text">
+      ${dataPhone[i].desc}
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</div>
+  `;
+}
+
+document.getElementById('productList').innerHTML = content;
